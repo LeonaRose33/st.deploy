e601f5421b237f49c45a2ccb85935f9ad7ec138d
LeonaRose33 <125284658+LeonaRose33@users.noreply.github.com>
2024-07-31 13:36:04 +0200

    Add files via upload

---
+++ portfolio_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd 
+import numpy as np
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</stle>', unsafe_allow_html=True)
+    
+st.title("Leona's Portfolio")
+st.write('Project title')
+st.write('Project title')
+st.write('Project title')
+st.write('Project title')
+st.write('Project title')
+st.write('Project title')
+st.write('Project title')
